402d8d3595bd3d375d9bffed37d10fc20cc9adba
zuhairzaidi63 <zuhair.zaidi407@gmail.com>
2024-03-17 17:21:27 +0500

    SignIN form completed

---
+++ Jquery.js
@@ -0,0 +1,26 @@
+
+$(function() {
+    $('#signInForm').on('submit',function(e) {
+        e.preventDefault();
+        var username = $('#username').val();
+        var password = $('#password').val();
+        
+        if (username.trim()===''){
+            // $('#textSignIN').show();
+            $('#textSignIN').text("*Username must be filled");
+        }
+        else{
+            $('#textSignIN').text("");
+            // $('#textSignIN').hide();
+        }
+        const pass = /(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*])[a-zA-Z\d!@#$%^&*]{8,}/;
+        if(password===''){
+            $('#passwordSignIN').text("*Password missing");
+        }
+        else if (!pass.test(password)) {
+            $('#passwordSignIN').text("*Strong passwrord (8 digit contain alphabet, numbers and special character ");
+        } else {
+            $('#passwordSignIN').text("");
+        }
+  });
+});
